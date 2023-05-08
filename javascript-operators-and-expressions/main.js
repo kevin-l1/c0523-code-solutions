91bc4565f4391e29eab81e743237c349b175b9d4
Kevin Li <likevinhp@gmail.com>
2023-05-08 22:12:27 +0000

    Completed javascript-operators-and-expressions exercise.

---
+++ javascript-operators-and-expressions/main.js
@@ -0,0 +1,45 @@
+const height = 85;
+const width = 45;
+const area = height * width;
+
+console.log('value of area:', area);
+console.log('typeof area:', typeof area);
+
+const bill = 1000;
+const payment = 1200;
+const change = payment - bill;
+
+console.log('value of change:', change);
+console.log('typeof change', typeof change);
+
+const quizzes = 80;
+const midterm = 90;
+const final = 95;
+const grade = (quizzes + midterm + final) / 3;
+
+console.log('value of grade:', grade);
+console.log('typeof grade:', typeof grade);
+
+const firstName = 'Kevin';
+const lastName = 'Li';
+const fullName = firstName + ' ' + lastName;
+
+console.log('value of fullName:', fullName);
+console.log('typeof fullName:', typeof fullName);
+
+const pH = 7;
+const isAcidic = pH < 7;
+
+console.log('value of isAcidic:', isAcidic);
+console.log('typeof isAcidic:', typeof isAcidic);
+
+const headCount = 88;
+const isSparta = headCount === 300;
+
+console.log('value of isSparta:', isSparta);
+console.log('typeof isSparta:', typeof isSparta);
+
+let motto = fullName;
+motto += ' is the GOAT';
+console.log('value of motto:', motto);
+console.log('typeof motto:', typeof motto);
