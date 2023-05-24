7ad314b16d610949ee0d63022e0ec931c6145a86
Kevin Li <likevinhp@gmail.com>
2023-05-24 18:44:36 +0000

    Fixed issues

--- dom-manipulation/main.js
+++ dom-manipulation/main.js
@@ -1,20 +1,21 @@
 let clickCounter = 0;
 const $hotBtn = document.querySelector('.hot-button');
 const $clkCount = document.querySelector('.click-count');
+
 $hotBtn.addEventListener('click', () => {
   clickCounter++
-  document.querySelector(".click-count").textContent = clickCounter;
+  $clkCount.textContent = clickCounter;
   if(clickCounter < 4) {
-  document.querySelector(".hot-button").className = "hot-button cold";
+  $hotBtn.className = "hot-button cold";
   } else if(clickCounter < 7) {
-  document.querySelector(".hot-button").className = "hot-button cool";
+  $hotBtn.className = "hot-button cool";
   } else if(clickCounter < 10) {
-  document.querySelector(".hot-button").className = "hot-button tepid";
+  $hotBtn.className = "hot-button tepid";
   } else if(clickCounter < 13) {
-  document.querySelector(".hot-button").className = "hot-button warm";
+  $hotBtn.className = "hot-button warm";
   } else if(clickCounter < 16) {
-  document.querySelector(".hot-button").className = "hot-button hot";
+  $hotBtn.className = "hot-button hot";
   } else {
-  document.querySelector(".hot-button").className = "hot-button nuclear";
+  $hotBtn.className = "hot-button nuclear";
   }
 })
