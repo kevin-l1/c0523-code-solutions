34279a244eb9784da2dde883ff96c7a6967cdef4
Kevin Li <likevinhp@gmail.com>
2023-05-26 21:37:22 +0000

    Completed javascript-local-storage

---
+++ javascript-local-storage/data.js
@@ -0,0 +1,10 @@
+/* exported todos */
+
+let todos = [];
+const previousTodosJSON = localStorage.getItem('javascript-local-storage');
+todos = JSON.parse(previousTodosJSON);
+
+window.addEventListener('beforeunload', () => {
+  const todosJSON = JSON.stringify(todos);
+  localStorage.setItem('javascript-local-storage', todosJSON);
+})
