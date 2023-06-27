27c7540eb9b88e011b8d6a11d539e1bc80c7d257
Kevin Li <likevinhp@gmail.com>
2023-06-27 20:01:24 +0000

    Completed react-rendering-lists

---
+++ react-rendering-lists/my-app/src/App.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+import PokemonList from './PokemonList';
+import { Pokemon } from './PokemonList';
+
+const pokedex: Pokemon[] = [
+  { number: '001', name: 'Bulbasaur' },
+  { number: '004', name: 'Charmander' },
+  { number: '007', name: 'Squirtle' },
+  { number: '025', name: 'Pikachu' },
+  { number: '039', name: 'Jigglypuff' },
+];
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <PokemonList pokedex={pokedex} />
+      </header>
+    </div>
+  );
+}
+
+export default App;
