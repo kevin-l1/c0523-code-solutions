486db58461cb20346ec44e86aef7a9ae425779b4
Kevin Li <likevinhp@gmail.com>
2023-06-28 04:29:21 +0000

    Completed react-form-controls

---
+++ react-form-controls/my-app/src/RegistrationFormControlled.js
@@ -0,0 +1,35 @@
+import { useState } from 'react';
+
+export default function RegistrationFormControlled() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(`Username: ${username}, Password: ${password}`);
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Username:
+        <input
+          type="text"
+          name="username"
+          onChange={(event) => setUsername(event.target.value)}
+        />
+      </label>
+
+      <label>
+        Password:
+        <input
+          type="password"
+          name="password"
+          onChange={(event) => setPassword(event.target.value)}
+        />
+      </label>
+
+      <button type="submit">Sign Up</button>
+    </form>
+  );
+}
