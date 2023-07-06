0d499ac783de9f04e90ed8f2aa2f1e7ecc5ea108
Kevin Li <likevinhp@gmail.com>
2023-07-06 04:26:18 +0000

    Completed react-accordion-component

--- react-accordion-component/my-app/src/Accordion.js
+++ react-accordion-component/my-app/src/Accordion.js
@@ -1,5 +1,41 @@
-export default function Accordion(topics){
-  return(
+import './Accordion.css';
 
-  )
+import { useState } from 'react';
+
+export default function Accordion({ topics }) {
+  const [openTopic, setOpenTopic] = useState();
+
+  function handleOpenTopic(topic) {
+    if (openTopic === topic) {
+      setOpenTopic(undefined);
+    } else {
+      setOpenTopic(topic);
+    }
+  }
+
+  const newArray = topics.map((topic) => (
+    <Topic
+      topic={topic}
+      isOpen={topic === openTopic}
+      onClick={() => handleOpenTopic(topic)}
+    />
+  ));
+
+  return <>{newArray}</>;
+}
+
+function Topic({ topic, isOpen, onClick }) {
+  return (
+    <div className="topic">
+      <div type="text" className="accordion-header" onClick={onClick}>
+        <h1>{topic.name}</h1>
+      </div>
+
+      {isOpen ? (
+        <div className="accordion-body">
+          <p>{topic.description}</p>
+        </div>
+      ) : null}
+    </div>
+  );
 }
--- react-accordion-component/my-app/src/App.tsx
+++ react-accordion-component/my-app/src/App.tsx
@@ -24,8 +24,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-          <Accordion topics={topics}/>
+        <Accordion topics={topics} />
       </header>
     </div>
   );
