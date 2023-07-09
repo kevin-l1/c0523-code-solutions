8b8b4177f92a69b429c41218bce452379f60074e
Kevin Li <likevinhp@gmail.com>
2023-07-09 05:24:40 +0000

    Completed react-app-drawer-component

--- react-app-drawer-component/my-app/src/AppDrawer.js
+++ react-app-drawer-component/my-app/src/AppDrawer.js
@@ -2,27 +2,39 @@ import './AppDrawer.css';
 import { useState } from 'react';
 
 export default function AppDrawer({ items }) {
+  const [active, setActive] = useState(false);
+  function handleHidden() {
+    setActive(!active);
+  }
+
   return (
     <>
-      <i class="fa-solid fa-bars"></i>
-      <div className="container">
-        <Menu />
-        <MenuItems items={items} />
+      <i class="fa-solid fa-bars" onClick={handleHidden}></i>
+      <div className={active ? 'container' : 'container hidden'}>
+        <Menu text="Hoyoverse Games" />
+        <MenuItems items={items} click={handleHidden} />
       </div>
+      <div
+        className={active ? 'overlay' : 'overlay hidden'}
+        onClick={handleHidden}></div>
     </>
   );
 }
 
-function Menu() {
+function Menu({ text }) {
   return (
     <div>
-      <h1>Menu</h1>
+      <h1>{text}</h1>
     </div>
   );
 }
 
-function MenuItems({ items }) {
-  let itemArray = items.map((e) => <div class="item">{e}</div>);
+function MenuItems({ items, click }) {
+  let itemArray = items.map((e) => (
+    <div className="item" onClick={click}>
+      <p>{e}</p>
+    </div>
+  ));
 
-  return <div>{itemArray}</div>;
+  return <div className="item-container">{itemArray}</div>;
 }
