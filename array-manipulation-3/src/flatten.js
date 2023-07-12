bc4192d88b51a8736c4620d7bcb7251dc966771a
Kevin Li <likevinhp@gmail.com>
2023-07-12 07:31:12 +0000

    update

--- array-manipulation-3/src/flatten.js
+++ array-manipulation-3/src/flatten.js
@@ -1,12 +1,4 @@
 /* exported flatten */
 function flatten(array) {
-  const newArray = array.map((e) => e);
-  for (let i = 0; i < array.length; i++) {
-    if (newArray[i].includes('[]')) {
-      console.log('hi');
-      newArray[i].remove('[]');
-    }
-  }
-  console.log(newArray);
-  return newArray;
+  return [].concat(...array);
 }
--- array-manipulation-3/src/unique.js
+++ array-manipulation-3/src/unique.js
@@ -1,1 +1,11 @@
 /* exported unique */
+function unique(array) {
+  const newArray = [];
+  for (let i = 0; i < array.length; i++) {
+    if (!newArray.includes(array[i])) {
+      newArray.push(array[i]);
+    }
+  }
+  console.log(newArray);
+  return newArray;
+}
