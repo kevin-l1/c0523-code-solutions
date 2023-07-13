8c6ee75b37195a5991129d6209784a5cce3c8ed0
Kevin Li <likevinhp@gmail.com>
2023-07-13 08:37:39 +0000

    Completed javascript-closures

--- javascript-closures/my-app/src/Counter.js
+++ javascript-closures/my-app/src/Counter.js
@@ -2,16 +2,16 @@ import React, { useEffect, useState } from 'react';
 import { useCallback } from 'react';
 
 export default function Counter() {
-  const [counter, setCounter] = useState(0);
-  useEffect(() => {
-    getData();
-    setCounter((prev) => prev + 1);
-  }, []);
-
   const getData = useCallback(() => {
     // fetch data
     return { foo: 'bar' };
   }, []);
+
+  const [counter, setCounter] = useState(0);
+  useEffect(() => {
+    getData();
+    setCounter((prev) => prev + 1);
+  }, [getData]);
 
   return <div>Fetching {counter} times</div>;
 }
