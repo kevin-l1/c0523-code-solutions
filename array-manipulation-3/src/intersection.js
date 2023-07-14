a92d4954fbe81845fc98d1c842e0e429ff095d3d
Kevin Li <likevinhp@gmail.com>
2023-07-14 06:32:27 +0000

    array-manipulation-3

--- array-manipulation-3/src/intersection.js
+++ array-manipulation-3/src/intersection.js
@@ -1,1 +1,10 @@
 /* exported intersection */
+function intersection(first, second) {
+  const newArray = [];
+  for (let i = 0; i < first.length; i++) {
+    if (second.includes(first[i])) {
+      newArray.push(first[i]);
+    }
+  }
+  return newArray;
+}
